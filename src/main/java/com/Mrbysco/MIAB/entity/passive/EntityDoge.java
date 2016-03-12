8f9a5e84f26e1a35ed752e7f1a60477c59b5779b
Mrbysco <Heeybysco@live.nl>
2016-03-12 20:11:14 +0100

    Forgot to remove
    I forgot to remove the sound calling from Doge. At the moment I don't
    have custom sounds for Doge yet.

--- src/main/java/com/Mrbysco/MIAB/entity/passive/EntityDoge.java
+++ src/main/java/com/Mrbysco/MIAB/entity/passive/EntityDoge.java
@@ -11,23 +11,5 @@ public class EntityDoge extends EntityWolf
 		public EntityDoge(World worldIn) {
 			super(worldIn);
 		}
-	
-		@Override
-	    protected String getLivingSound()
-	    {
-	        return this.isAngry() ? "miab:Doge.growl" : (this.rand.nextInt(3) == 0 ? (this.isTamed() && this.dataWatcher.getWatchableObjectFloat(18) < 10.0F ? "miab:Doge.whine" : "miab:Doge.panting") : "miab:Doge.bark");
-	    }
-		
-		@Override
-	    protected String getHurtSound()
-	    {
-	        return "miab:Doge.hurt";
-	    }
-		
-		@Override
-	    protected String getDeathSound()
-	    {
-	        return "miab:Doge.death";
-	    }
 
 	}
